c8e885933ca6c9e3eb13bcd939c2a91577723bb1
Yan Ma <yma@wayfair.com>
2016-07-10 12:52:16 -0400

    change payback call content

--- index.js
+++ index.js
@@ -953,7 +953,9 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+
+  // sendTextMessage(senderID, "Postback called");
+  sendTextMessage(senderID, payload);
 }
 
 /*
