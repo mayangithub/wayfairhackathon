caf13f21ef8bd6a1982a3f361633b7b5d92b5b71
Wesley Abbey <wabbey@wayfair.com>
2016-07-09 23:09:48 -0400

    added to lower case for all options

--- index.js
+++ index.js
@@ -366,7 +366,7 @@ function receivedMessage(event) {
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
-    switch (messageText) {
+    switch (messageText.toLowerCase()) {
       case 'lucky':
         sendBestSellersMessage(senderID);
         break;
